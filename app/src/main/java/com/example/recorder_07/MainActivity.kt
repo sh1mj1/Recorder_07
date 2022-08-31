268c8952e7f8a223f2f5fbe667e53d82d9ad4da3
sh1mj1 <7wlgns@gmail.com>
2022-08-31 18:20:56 +0900

    show amplitude, but not including reset

--- app/src/main/java/com/example/recorder_07/MainActivity.kt
+++ app/src/main/java/com/example/recorder_07/MainActivity.kt
@@ -2,25 +2,47 @@ package com.example.recorder_07
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     // Mark -Properties/////////////////////////////////////////
+    private val soundVisualizerView: SoundVisualizerView by lazy { findViewById(R.id.soundVisualizer_V) }
+    private val resetBtn: Button by lazy { findViewById(R.id.reset_Btn) }
     private val recordBtn: RecordBtn by lazy { findViewById(R.id.record_Btn) }
     private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var state = State.BEFORE_RECORDING
+    set(value) {
+        field = value
+        resetBtn.isEnabled = (value == State.AFTER_RECORDING) ||
+                (value == State.ON_PLAYING)
+        recordBtn.updateIconWithState(value)
+    }
 
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var player: MediaPlayer? = null
+    private var recorder: MediaRecorder? = null
+
+
+
+    // Mark -LifeCycle/////////////////////////////////////////
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         requestAudioPermission()
         initViews()
+        bindViews()
+        initVariables()
 
     }
 
@@ -43,12 +65,92 @@ class MainActivity : AppCompatActivity() {
 
     // Mark -Help/////////////////////////////////////////
 
+    private fun initVariables() {
+        state = State.BEFORE_RECORDING
+    }
+
+    private fun startRecording(){
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            // 지금 구현하는 녹음기 같은 경우 녹음을 하고 확인하고 따로 저장하지 않는 형태를 띄고 있기 때문에
+            // 앱에서만 접근할 수 있는 스토리지는 범위가 한정되어 있음 scope Storage. 내부 & 외부
+            // 인터널 같은 경우는 용량이 한정되어 있음. (녹음 파일을 저장하기 에는)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        soundVisualizerView.startVisualizing()
+        state = State.ON_RECORDING
+        Log.d("AudioState", "AudioState: $state")
+    }
+    private fun stopRecording(){
+        recorder?.run{
+            stop()
+            release()
+        }
+        recorder = null
+        soundVisualizerView.stopVisualizing()
+        state = State.AFTER_RECORDING
+        Log.d("AudioState", "AudioState: $state")
+    }
+
+    private fun startPlaying(){
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            // 로딩할 때 시간이 오래 걸린다거나 스트리밍 같은 경우 서버에서 데이터를 가져오는 것이므로 aync 을 사용함.
+            //  또 로딩 중 로딩인디케이터같은 UI 를 사용. 하지만 우리는 단지 방금 녹음된 거승ㄹ 가져오는 것 뿐이기 때문에 async 하지 않으 ㄱ서임.
+            prepare()
+        }
+        player?.start()
+        state = State.ON_PLAYING
+        Log.d("AudioState", "AudioState: $state")
+    }
+
+    private fun stopPlaying(){
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
+        Log.d("AudioState", "AudioState: $state")
+    }
+
     private fun requestAudioPermission() {
         requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initViews() {
         recordBtn.updateIconWithState(state)
+    }
+
+    private fun bindViews(){
+        // 사운드비주얼라이저뷰 클래스에서 visualizeRepeatAction 이 반복 동작할 때
+        soundVisualizerView.onRequestCurrentAmplitude = {
+            recorder?.maxAmplitude ?: 0
+        }
+
+        resetBtn.setOnClickListener {
+            stopPlaying()
+            state = State.BEFORE_RECORDING
+        }
+
+        recordBtn.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/recorder_07/SoundVisualizerView.kt
@@ -0,0 +1,102 @@
+package com.example.recorder_07
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import kotlin.random.Random
+
+class SoundVisualizerView (
+    context: Context,
+    attrs: AttributeSet? = null): View(context, attrs) {
+
+    // Mark -Properties/////////////////////////////////////////
+    var onRequestCurrentAmplitude: ( () -> Int )? = null
+
+    private val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.purple_200)
+        strokeWidth = LINE_WIDTH
+        strokeCap = Paint.Cap.ROUND
+    } // 계단화 방지
+
+    private var drawingWidth: Int = 0
+    private var drawingHeight: Int = 0
+    private var drawingAmplitudes: List<Int> = emptyList()
+    // For test (10번 최대값까지 임의 수를 리스트에 넣기)
+//    var drawingAmplitudes: List<Int> = (0..10).map{ Random.nextInt(Short.MAX_VALUE.toInt())}
+
+
+    private val visualizeRepeatAction: Runnable = object: Runnable {
+        override fun run() {
+            // TODO: Amplitude 가져오고 Draw 요청 \
+            val currentAmplitude = onRequestCurrentAmplitude?.invoke() ?:0
+
+            drawingAmplitudes = listOf(currentAmplitude) + drawingAmplitudes
+            invalidate() // 데이터가 초기화 되었을 때 이걸 호출 해야 onDraw 가 다시 호출됨. 호출하지 않으면 데이터는 계속 추가되는데 뷰가 갱신이 안된다.
+            handler?.postDelayed(this, 20L)
+
+        }
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        drawingWidth = w
+        drawingHeight = h
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas ?: return
+
+        val centerY = drawingHeight / 2f
+        // 어떤걸 그려야 할지.
+
+        var offsetX = drawingWidth.toFloat()
+        // 수많은 진폭값들을 리스트의 원소로 오른쪽부터 접근할 거임. (drawingAmplitudes)
+        // (UI가 오른쪽부터 왼쪽으로 이동하는 느낌으로 보이니까)
+        drawingAmplitudes.forEach {amplitude ->
+            val lineLength = amplitude / MAX_AMPLITUDE * drawingHeight * 0.8F
+
+            offsetX -= LINE_SPACE
+            // 녹음을 길게하면 분명히 뒤에서 다 못 그리는 amplitude 가 발생할 것임.
+            // 화면에 보이지 않는다면 forEach 루프를 벗어나도록 하자.
+            if (offsetX < 0) return@forEach
+
+            canvas.drawLine(
+                offsetX,
+                centerY - lineLength / 2F,
+                offsetX,
+                centerY + lineLength / 2F,
+                amplitudePaint
+            )
+
+        }
+
+
+
+    }
+
+    fun startVisualizing() {
+        handler?.post(visualizeRepeatAction)
+    }
+
+    fun stopVisualizing() {
+        handler?.removeCallbacks(visualizeRepeatAction)
+    }
+
+
+    companion object {
+        private const val LINE_WIDTH = 10F
+        private const val LINE_SPACE = 15F
+        // amplitude 라는 메서드를 사용할 것임. 그 때 리턴되는 최대값이 32,767 : Short
+        // Int 값을 이 32,767 로 나누면 값이 0이 됨. 그래서 Float() 타입으로 변경..
+        // 왜지?
+        private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat()
+        private const val ACTION_INTERVAL = 20L
+    }
+
+}
